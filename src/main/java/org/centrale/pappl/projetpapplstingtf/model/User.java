9e9b656b6b442d843164b91bb1f08fa1676e8e9e
Santiago Alexander <srodriguezt@unal.edu.co>
2025-10-26 10:40:28 +0100

    Adding configurations and github actions before review

---
+++ src/main/java/org/centrale/pappl/projetpapplstingtf/model/User.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.pappl.projetpapplstingtf.model;
+
+/**
+ *
+ * @author srodr
+ */
+public class User {
+    
+    String mail;
+    String login;
+    String hashPassword; 
+
+    public User(String mail, String login, String hashPassword) {
+        this.mail = mail;
+        this.login = login;
+        this.hashPassword = hashPassword;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getHashPassword() {
+        return hashPassword;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+    
+    
+    
+}
---
+++ src/main/java/org/centrale/pappl/projetpapplstingtf/service/LoginService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.pappl.projetpapplstingtf.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author srodr
+ */
+@Service
+public class LoginService {
+    
+}
