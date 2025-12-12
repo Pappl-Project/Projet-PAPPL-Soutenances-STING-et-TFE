7024490bac742e7912574967d0e0c58ffe96933d
Trianon27 <srodriguezt@unal.edu.co>
2025-12-12 23:52:07 +0100

    Fix: CI pipeline setup with separated test environment config

--- src/main/java/org/centrale/pappl/projetpapplstingtf/config/RootConfig.java
+++ src/main/java/org/centrale/pappl/projetpapplstingtf/config/RootConfig.java
@@ -18,7 +18,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
  * @author srodr
  */
 @Configuration
-@PropertySource(value = "classpath:db.properties", ignoreResourceNotFound = true)
+@PropertySource(value = "classpath:db.properties")
 @ComponentScan(basePackages = "org.centrale.pappl.projetpapplstingtf") //IT MUST READ ALSO THE PROPERTIES
 public class RootConfig {
     
